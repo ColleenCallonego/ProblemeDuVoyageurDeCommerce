bf5e28b4c3aaaaa4aa59d83887afe80500664e8e
Vellucen <gabriel.couroux33@orange.fr>
2019-12-16 13:54:01 +0100

    Test de Gaby, il vérifie qu'il sait utiliser git sur NetBeans

--- ProblemeDuVoyageurDeCommerce/src/Main/Main.java
+++ ProblemeDuVoyageurDeCommerce/src/Main/Main.java
@@ -1,9 +1,10 @@
-package problemeduvoyageurdecommerce;
+package Main;
 
 public class Main {
 
 
     public static void main(String[] args) {
+        //teste on va voir si gaby sait utiliser git sur netbeans
     }
     
 }
