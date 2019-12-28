9b63d99dd8634c061c23586fc1e6143496f6e34b
ColleenCallonego <colleencallonego@gmail.com>
2019-12-28 13:33:30 +0100

    Création de la matrice distance à partir du fichier JSON

--- ProblemeDuVoyageurDeCommerce/src/Main/Main.java
+++ ProblemeDuVoyageurDeCommerce/src/Main/Main.java
@@ -1,10 +1,13 @@
 package Main;
 
+import Fichier.Lecture;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
 
-
-    public static void main(String[] args) {
-        //teste on va voir si gaby sait utiliser git sur netbeans
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        Lecture.creationMatrice();
     }
     
 }
---
+++ ProblemeDuVoyageurDeCommerce/src/Ville/Ville.java
@@ -0,0 +1,9 @@
+package Ville;
+
+public class Ville {
+    public String nom;
+
+    public Ville(String nom) {
+        this.nom = nom;
+    }
+}
