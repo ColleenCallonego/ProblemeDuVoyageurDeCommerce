7ec4720df1d9c1e8d30ec1a6200b875a752510ed
ColleenCallonego <colleencallonego@gmail.com>
2019-12-30 15:51:43 +0100

    Supression package genetique

--- ProblemeDuVoyageurDeCommerce/src/Genetique/Selection.java
+++
@@ -1,5 +0,0 @@
-package Genetique;
-
-public class Selection {
-    
-}
--- ProblemeDuVoyageurDeCommerce/src/Ville/Matrice.java
+++ ProblemeDuVoyageurDeCommerce/src/Ville/Matrice.java
@@ -1,10 +1,25 @@
 package Ville;
 import java.util.ArrayList;
 
+/**
+ *Classe qui correspond à toutes les informations nécéssaires à propos 
+ * des distances et des villes. 
+ * On peut y trouver une ArrayList de String et un tableau/matrice en deux 
+ * dimensions.
+ * On s'aide de l'ArrayList pour savoir à quelle ville correpond un indice dans 
+ * le tableau.
+ * L'indice d'une ville dans l'ArrayList correspond donc à l'indice de cette 
+ * même ville dans le tableau/matrice. 
+ */
 public class Matrice {
-    public ArrayList<String> villes;
-    public Integer[][] matrice;
+    public ArrayList<String> villes; //ArrayList des villes à visiter
+    public Integer[][] matrice; //Tableau/matrice des distances entre toutes les villes
 
+    /**
+     *Constructeur de la classe Matrice.
+     * @param matrice Tableau/matrice créé(e) où l'on retrouve toutes les distances.
+     * @param villes ArrayList de toutes les villes.
+     */
     public Matrice(Integer[][] matrice, ArrayList<String> villes) {
         this.villes = villes;
         this.matrice = matrice;
